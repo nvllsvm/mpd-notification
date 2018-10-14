bf8e4bc8d2723d7cb02c988367acf5ed22985ece
Andrew Rabert <ar@nullsum.net>
2018-10-14 18:53:08 -0400

    Simplify notification text more

--- config.def.h
+++ config.def.h
@@ -17,10 +17,10 @@
 /* strings used to display notification messages
  * TEXT_PLAY_* need to include one string modifier '%s' each. */
 #define TEXT_TOPIC		"MPD Notification"
-#define TEXT_PLAY_PAUSE_STATE	"%s "
+#define TEXT_PLAY_PAUSE_STATE	"%s"
 #define TEXT_PLAY_PAUSE_TITLE	"<b>%s</b>"
 #define TEXT_PLAY_PAUSE_ARTIST	"<i>%s</i>"
-#define TEXT_PLAY_PAUSE_ALBUM	"<i>%s</i>"
+#define TEXT_PLAY_PAUSE_ALBUM	""
 #define TEXT_STOP		"Stopped playback"
 #define TEXT_NONE		"No action received yet."
 #define TEXT_UNKNOWN		"(unknown)"
